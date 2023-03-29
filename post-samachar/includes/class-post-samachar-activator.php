6139d0ba9423e31ae9b67296e55ecfc88b9a0905
chaudhari-mithilesh <mithilesh.chaudhari@wisdmlabs.com>
2023-03-29 09:30:25 +0530

    Template files from WPPB. Nothing was changed here.

---
+++ post-samachar/includes/class-post-samachar-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://mithilesh.wisdmlabs.net/
+ * @since      1.0.0
+ *
+ * @package    Post_Samachar
+ * @subpackage Post_Samachar/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Post_Samachar
+ * @subpackage Post_Samachar/includes
+ * @author     Mithilesh <mithilesh.chaudhaudhari@wisdmlabs.com>
+ */
+class Post_Samachar_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ post-samachar/includes/class-post-samachar-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://https://mithilesh.wisdmlabs.net/
+ * @since      1.0.0
+ *
+ * @package    Post_Samachar
+ * @subpackage Post_Samachar/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Post_Samachar
+ * @subpackage Post_Samachar/includes
+ * @author     Mithilesh <mithilesh.chaudhaudhari@wisdmlabs.com>
+ */
+class Post_Samachar_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
